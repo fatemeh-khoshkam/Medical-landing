8aea4d3a329c5049625a571156511afec71b1957
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-06 21:46:31 +0430

    update style1

--- js/script.js
+++ js/script.js
@@ -33,3 +33,25 @@ popularSearchList.forEach((item, index) => {
         popularSearchContent[index].classList.add('active');
     })
 });
+
+
+/***  mobile menu ***/
+const menu = document.querySelector('#menu');
+const menuOpen = document.querySelector('#nav-open');
+const menuClosed = menu.querySelector('#nav-close');
+const menuOverlay = document.querySelector('.overlay');
+
+
+menuOpen.addEventListener('click', () => {
+    toggleMenu();
+});
+menuClosed.addEventListener('click', () => {
+    toggleMenu();
+});
+menuOverlay.addEventListener('click', () => {
+    toggleMenu();
+});
+function toggleMenu() {
+    menu.classList.toggle('nav__right--open');
+    menuOverlay.classList.toggle('active');
+}
