405fc1d2a8263998c67ac7b0df5b797e987d7be2
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-07 20:37:58 +0430

    update style8

--- js/script.js
+++ js/script.js
@@ -65,3 +65,17 @@ function toggleMenu() {
     menu.classList.toggle('nav__right--open');
     menuOverlay.classList.toggle('active');
 }
+
+/***  slider ***/
+var swiper = new Swiper('.mySwiper', {
+    speed: 600,
+    parallax: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
